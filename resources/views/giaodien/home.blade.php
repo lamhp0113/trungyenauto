6dbb52207366eb5d232a554830bdcec3b9a56150
tiepna <nguyenanhtiep@gmail.com>
2021-05-27 17:18:02 +0700

    update footer

--- resources/views/giaodien/home.blade.php
+++ resources/views/giaodien/home.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 @include('giaodien.layouts.style')
-  
+
 </head>
 <body>
 @include('giaodien.layouts.main-header')
@@ -50,7 +50,7 @@
 <section class="intro">
 
 </div>
-</div>	
+</div>
 </section>
 
 <section class="ftco-section">
@@ -68,7 +68,7 @@
         <span class="flaticon-car-service"></span>
       </div>
       <div class="media-body pl-3">
-        <a href="#"><h3 class="heading">Bão dưỡng oto định kỳ</h3></a>
+        <a href="#"><h3 class="heading">Bão dưỡng ô tô định kỳ</h3></a>
         <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
         <p><a href="#" class="btn-custom">Read more</a></p>
       </div>
@@ -78,11 +78,11 @@
         <span class="flaticon-tyre"></span>
       </div>
       <div class="media-body pl-3">
-        <a href="#"><h3 class="heading">Đại tu động cơ oto</h3></a>
+        <a href="#"><h3 class="heading">Đại tu động cơ ô tô</h3></a>
         <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
         <p><a href="#" class="btn-custom">Read more</a></p>
       </div>
-    </div> 
+    </div>
 
   </div>
   <div class="col-md-4 services ftco-animate">
@@ -91,21 +91,21 @@
         <span class="flaticon-battery"></span>
       </div>
       <div class="media-body pl-3">
-        <a href="#"><h3 class="heading">Bão dưỡng oto định kỳ</h3></a>
+        <a href="#"><h3 class="heading">Bão dưỡng ô tô định kỳ</h3></a>
         <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
         <p><a href="#" class="btn-custom">Read more</a></p>
       </div>
-    </div> 
+    </div>
     <div class="d-block d-flex">
       <div class="icon d-flex justify-content-center align-items-center">
         <span class="flaticon-car-engine"></span>
       </div>
       <div class="media-body pl-3">
-       <a href="#"><h3 class="heading">Bão dưỡng oto định kỳ</h3></a>
+       <a href="#"><h3 class="heading">Bão dưỡng ô tô định kỳ</h3></a>
        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
        <p><a href="#" class="btn-custom">Read more</a></p>
      </div>
-   </div>    
+   </div>
  </div>
 
  <div class="col-md-4 services ftco-animate">
@@ -114,7 +114,7 @@
       <span class="flaticon-tow-truck"></span>
     </div>
     <div class="media-body pl-3">
-      <a href="#"><h3 class="heading">Bão dưỡng oto định kỳ</h3></a>
+      <a href="#"><h3 class="heading">Bão dưỡng ô tô định kỳ</h3></a>
       <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
       <p><a href="#" class="btn-custom">Read more</a></p>
     </div>
@@ -124,11 +124,11 @@
       <span class="flaticon-repair"></span>
     </div>
     <div class="media-body pl-3">
-     <a href="#"><h3 class="heading">Bão dưỡng oto định kỳ</h3></a>
+     <a href="#"><h3 class="heading">Bão dưỡng ô tô định kỳ</h3></a>
      <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
      <p><a href="#" class="btn-custom">Read more</a></p>
    </div>
- </div>       
+ </div>
 </div>
 </div>
 </div>
@@ -138,8 +138,8 @@
  <div class="container">
   <div class="row d-md-flex justify-content-end">
    <div class="col-md-12 col-lg-6 half p-3 py-5 pl-lg-5 ftco-animate heading-section heading-section-white">
-    <span class="subheading">Booking an Appointment</span>
-    <h2 class="mb-4">Free Consultation</h2>
+    <span class="subheading">Đặt lịch bảo dưỡng và sửa chữa</span>
+    <h2 class="mb-4">Miễn phí tư vấn</h2>
     <form action="#" class="appointment">
      <div class="row">
       <div class="col-md-12">
@@ -148,7 +148,7 @@
           <div class="select-wrap">
            <div class="icon"><span class="fa fa-chevron-down"></span></div>
            <select name="" id="" class="form-control">
-            <option value="">Select services</option>
+            <option value="">Chọn dịch vụ</option>
             <option value="">Change Oil</option>
             <option value="">Engine Repair</option>
             <option value="">Battery Replace</option>
@@ -161,38 +161,30 @@
   </div>
   <div class="col-md-6">
    <div class="form-group">
-     <input type="text" class="form-control" placeholder="Your Name">
+     <input type="text" class="form-control" placeholder="Tên Bạn">
    </div>
  </div>
  <div class="col-md-6">
    <div class="form-group">
-     <input type="text" class="form-control" placeholder="Vehicle number">
+     <input type="text" class="form-control" placeholder="Loại xe">
    </div>
  </div>
- <div class="col-md-6">
+ <div class="col-md-12">
    <div class="form-group">
     <div class="input-wrap">
      <div class="icon"><span class="fa fa-calendar"></span></div>
-     <input type="text" class="form-control appointment_date" placeholder="Date">
+     <input type="text" class="form-control appointment_date" placeholder="Ngày đặt">
    </div>
  </div>
 </div>
-<div class="col-md-6">
- <div class="form-group">
-  <div class="input-wrap">
-   <div class="icon"><span class="fa fa-clock-o"></span></div>
-   <input type="text" class="form-control appointment_time" placeholder="Time">
- </div>
-</div>
-</div>
 <div class="col-md-12">
  <div class="form-group">
-   <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+   <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Ghi Chú"></textarea>
  </div>
 </div>
 <div class="col-md-12">
  <div class="form-group">
-   <input type="submit" value="Send message" class="btn btn-dark py-3 px-4">
+   <input type="submit" value="Đặt lịch" class="btn btn-dark py-3 px-4">
  </div>
 </div>
 </div>
@@ -273,84 +265,8 @@
 </div>
 </section>
 
-
-
-
-<footer class="footer ftco-section">
-  <div class="container">
-    <div class="row mb-5">
-      <div class="col-md-6 col-lg">
-        <div class="ftco-footer-widget mb-4">
-          <h2 class="logo"><a href="#">Autorepair<span>.</span></a></h2>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-          <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-4">
-            <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
-            <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
-            <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-6 col-lg">
-        <div class="ftco-footer-widget mb-4 ml-md-5">
-          <h2 class="ftco-heading-2">Services</h2>
-          <ul class="list-unstyled">
-            <li><a href="#" class="py-1 d-block"><span class="fa fa-check mr-3"></span>Oil Change</a></li>
-            <li><a href="#" class="py-1 d-block"><span class="fa fa-check mr-3"></span>Batteries</a></li>
-            <li><a href="#" class="py-1 d-block"><span class="fa fa-check mr-3"></span>Tow Truck</a></li>
-            <li><a href="#" class="py-1 d-block"><span class="fa fa-check mr-3"></span>Tire Change</a></li>
-            <li><a href="#" class="py-1 d-block"><span class="fa fa-check mr-3"></span>Engine Repair</a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-6 col-lg">
-        <div class="ftco-footer-widget mb-4">
-         <h2 class="ftco-heading-2">Contact information</h2>
-         <div class="block-23 mb-3">
-           <ul>
-             <li><span class="icon fa fa-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-             <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-             <li><a href="#"><span class="icon fa fa-paper-plane"></span><span class="text">info@yourdomain.com</span></a></li>
-           </ul>
-         </div>
-       </div>
-     </div>
-     <div class="col-md-6 col-lg">
-       <div class="ftco-footer-widget mb-4">
-        <h2 class="ftco-heading-2">Business Hours</h2>
-        <div class="opening-hours">
-         <h4>Opening Days:</h4>
-         <p class="pl-3">
-          <span>Monday – Friday : 9am to 20 pm</span>
-          <span>Saturday : 9am to 17 pm</span>
-        </p>
-        <h4>Vacations:</h4>
-        <p class="pl-3">
-          <span>All Sunday Days</span>
-          <span>All Official Holidays</span>
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="row">
-  <div class="col-md-12 text-center">
-
-    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-      Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-      <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-    </div>
-  </div>
-</div>
-</footer>
-
-
-
-<!-- loader -->
-
-
-
+<!-- footer -->
+@include('giaodien.layouts.footer')
 @include('giaodien.layouts.script')
-
-
 </body>
 </html>
