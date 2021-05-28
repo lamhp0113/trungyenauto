ec0754b7797c1d1c642994088b5d4803de118fb5
tung lam <lamhp0113@gmail.com>
2021-05-28 15:00:54 +0700

    serivces tạo mới

---
+++ app/Http/Controllers/ServicesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Booking;
+
+class BookingController extends Controller
+{
+    public function index()
+    {
+        $orders = Booking::latest()->paginate(1);
+        return view('admin.booking.index', compact('orders'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    public function store(Request $request)
+    {
+        Booking::create($request->all());
+        return redirect()->route('projects.index')
+            ->with('success', 'Đặt lịch thành công.');
+    }
+
+    public function edit(Booking $booking)
+    {
+        return view('admin.booking.update', compact('booking'));
+    }
+
+    public function update(Request $request, Booking $booking)
+    {
+        $booking->update($request->all());
+        return redirect()->route('booking.index')
+            ->with('success', 'Cập nhập thành công');
+    }
+}
---
+++ app/Models/Services.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $table = 'booking';
+    public $timestamps = true;
+    protected $fillable = [
+        'customer_name',
+        'phone',
+        'email',
+        'booking_time',
+        'status',
+        'customer_comment',
+        'remark'
+    ];
+}
