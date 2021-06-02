32d21488fd6098d514834223afa9482483602445
tiepna <nguyenanhtiep@gmail.com>
2021-06-03 06:51:18 +0700

    remove unsued lib

--- resources/views/giaodien/layouts/script.blade.php
+++ resources/views/giaodien/layouts/script.blade.php
@@ -11,6 +11,4 @@
 <script src="{{asset('js/owl.carousel.min.js')}}"></script>
 <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
 <script src="{{asset('js/scrollax.min.js')}}"></script>
-<script src="{{asset('https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false')}}"></script>
-<script src="{{asset('js/google-map.js')}}"></script>
 <script src="{{asset('js/main.js')}}"></script>
